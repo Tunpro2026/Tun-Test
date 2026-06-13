4dd7a2c615b795eb93b77daacf2b80c5bd7a73d7
Tunpro2026 <tunlay2309@gmail.com>
2026-06-13 23:56:01 +0630

    Create my_script.py

---
+++ my_script.py
@@ -0,0 +1,7 @@
+print("--- Starting Python Cloud Calculator ---")
+
+for i in range(1, 6):
+    result = i * 10
+    print(f"Calculation Step {i}: Input is {i} | Output is {result}")
+
+print("--- Calculation Completed Successfully ---")
